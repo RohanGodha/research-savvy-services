411b889ea71e6603a90c0105188df593645f7517
Rohan Godha <yoyorodha@gmail.com>
2023-10-15 23:21:19 +0530

    Create About.svelte

---
+++ src/About.svelte
@@ -0,0 +1,33 @@
+<script>
+  let name = 'Your Name';
+  let description = 'Write a brief description about yourself here.';
+</script>
+
+<div class="about-container">
+  <h2>About Me</h2>
+  <h3>{name}</h3>
+  <p>{description}</p>
+</div>
+
+<style>
+  .about-container {
+    text-align: center;
+    margin: 20px;
+  }
+
+  h2 {
+    font-size: 24px;
+  }
+
+  h3 {
+    font-size: 20px;
+    color: #333;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 16px;
+    color: #555;
+    line-height: 1.6;
+  }
+</style>
